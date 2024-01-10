19ebb44a39b0d5f32a679835595998833da74727
Zero Tang <zero.tangptr@gmail.com>
2024-01-10 02:05:10 -0500

    Update by Jan.10th, 2024
    Fix warnings.
    Remove warning suppressor.

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ set incpath=T:\Program Files\Windows Kits\10\Include\10.0.22000.0
 set libpath=T:\Program Files\Windows Kits\10\Lib\10.0.22000.0
 
 echo Compiling...
-cl snprintf.c /I"%incpath%\shared" /I"%incpath%\um" /I"%incpath%\ucrt" /I"%ddkpath%\include" /Zi /nologo /W3 /WX /wd4267 /wd4244 /Od /D"HAVE_STDARG_H" /D"HAVE_LOCALE_H" /D"HAVE_STDDEF_H" /D"HAVE_FLOAT_H" /D"HAVE_STDINT_H" /D"HAVE_INTTYPES_H" /D"HAVE_LONG_LONG_INT" /D"HAVE_UNSIGNED_LONG_LONG_INT" /D"HAVE_ASPRINTF" /D"HAVE_VASPRINTF" /D"HAVE_SNPRINTF" /Zc:wchar_t /std:c17 /FAcs /Fa"snprintf.cod" /Fo"snprintf.obj" /Fd"vc142.pdb" /GS- /Qspectre /TC /c /errorReport:queue
+cl snprintf.c /I"%incpath%\shared" /I"%incpath%\um" /I"%incpath%\ucrt" /I"%ddkpath%\include" /Zi /nologo /W3 /WX /Od /D"HAVE_STDARG_H" /D"HAVE_LOCALE_H" /D"HAVE_STDDEF_H" /D"HAVE_FLOAT_H" /D"HAVE_STDINT_H" /D"HAVE_INTTYPES_H" /D"HAVE_LONG_LONG_INT" /D"HAVE_UNSIGNED_LONG_LONG_INT" /D"HAVE_ASPRINTF" /D"HAVE_VASPRINTF" /D"HAVE_SNPRINTF" /Zc:wchar_t /std:c17 /FAcs /Fa"snprintf.cod" /Fo"snprintf.obj" /Fd"vc142.pdb" /GS- /Qspectre /TC /c /errorReport:queue
 
 cl tester.c /I"%incpath%\shared" /I"%incpath%\um" /I"%incpath%\ucrt" /I"%ddkpath%\include" /Zi /nologo /W3 /WX /Od /D"HAVE_STDARG_H" /Zc:wchar_t /std:c17 /FAcs /Fa"tester.cod" /Fo"tester.obj" /Fd"vc142.pdb" /GS- /Qspectre /TC /c /errorReport:queue
 
